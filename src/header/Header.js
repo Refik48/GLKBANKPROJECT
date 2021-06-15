50a0ffe1e21ba55202bda48273685de00e88847c
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-06-15 22:35:24 +0100

    15.06

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from "react-router-dom";
-import logo from "../images/logo.png";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,24 +15,26 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  button: {
+    color: "white",
+    fontSize: "15px",
+    fontWeight: "bold",
+  },
   title: {
     flexGrow: 1,
   },
 }));
-
 export default function Header() {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-         <Link to="/">
-         <img src={logo} alt="logo" />
-         </Link>
-         
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
           <Typography variant="h6" className={classes.title}>
-            GLK BANK
+            MTD BANK
           </Typography>
           <Button className={classes.button}>ABOUT US</Button>
         </Toolbar>
@@ -42,5 +42,3 @@ export default function Header() {
     </div>
   );
 }
-
-
